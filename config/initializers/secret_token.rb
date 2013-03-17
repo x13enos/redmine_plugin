992c79b67ec3b0c456a82a1466802d3d7478d9d9
x3enos <x3enos@gmail.com>
2013-03-17 21:57:21 +0200

    [^] change name app in secret token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,4 +5,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Redmine::Application.config.secret_token = '090a334dc04dd684ecef7cda2fe011febc789d03093fcceb74ee9de12e49fd5236d5ead20338ff1b72bb2117bbc932f0d5547ee09f4cb7d81fb05935f1c484d0'
+RedmineApp::Application.config.secret_token = '090a334dc04dd684ecef7cda2fe011febc789d03093fcceb74ee9de12e49fd5236d5ead20338ff1b72bb2117bbc932f0d5547ee09f4cb7d81fb05935f1c484d0'
